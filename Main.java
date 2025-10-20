f25616cec136cc8fc4e4f2c385d0f95700930340
NotFlash <geraldoagha28@gmail.com>
2025-10-20 09:48:05 +0700

    JAVAAA

--- Main.java
+++ Main.java
@@ -3,5 +3,8 @@ package Modul3.CodeLab1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World1");
+        System.out.println("Hello World2");
+        System.out.println("Hello World3");
     }
 }
